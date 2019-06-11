b4ffc32c3c9fa0fbb566359bd05550d1d96bbe66
malinac <malina.catanoiu@gmail.com>
2019-06-11 09:14:07 +0300

    json/xml export done

--- export.php
+++ export.php
@@ -7,7 +7,8 @@ $mysql = new mysqli (
     'sour'
 );
 $fn = fopen("newfile.txt", "r");
-if ($option = 'CSV') {
+
+if ($option == 'CSV') {
     $nf = fopen("products.csv", "w") or die("Unable to open file!");
     while(! feof($fn)) {
     $id = trim(fgets($fn));
@@ -24,9 +25,11 @@ if ($option = 'CSV') {
 
         }
     }
+    fclose($nf);
+    echo file_get_contents("products.csv");
 }
-/*
-else if($option = 'JSON') {
+
+elseif($option == 'JSON') {
     $nf = fopen("products.json", "w") or die("Unable to open file!");
     while (!feof($fn)) {
         $id = trim(fgets($fn));
@@ -34,19 +37,46 @@ else if($option = 'JSON') {
             $sql = "SELECT * FROM PRODUCTS where id =" . $id;
             $result = mysqli_query($mysql, $sql);
             $row = mysqli_fetch_row($result);
-            fwrite($nf, '[');
             if ($result === false) {
                 echo "error while executing mysql: " . mysqli_error($mysql);
             } else {
                 $s_array[] = array('id' => $row[0],
                     'name' => $row[1]);
-                fwrite($nf, json_encode($s_array));
             }
         }
-
     }
+    $jsone = json_encode($s_array);
+    fwrite($nf, $jsone);
+    fclose($nf);
+    echo file_get_contents("products.json");
 }
-*/
-fclose($nf);
+
+elseif($option == 'XML'){
+    $xml=new DOMDocument("1.0");
+    $xml->formatOutput=true;
+    $top=$xml->createElement("List_of_Recomandations");
+    $xml->appendChild($top);
+    while (!feof($fn)) {
+        $id = trim(fgets($fn));
+        if (!empty($id)) {
+            $sql = "SELECT * FROM PRODUCTS where id =" . $id;
+            $result = mysqli_query($mysql, $sql);
+            $row = mysqli_fetch_row($result);
+            if ($result === false) {
+                echo "error while executing mysql: " . mysqli_error($mysql);
+            } else {
+                $product = $xml->createElement("product");
+                $top->appendChild($product);
+                $name = $xml->createElement("name", $row[1]);
+                $product->appendChild($name);
+                $price = $xml->createElement("price", $row[4]);
+                $product->appendChild($price);
+            }
+        }
+    }
+    echo "<xmp>".$xml->saveXML()."</xmp>";
+}
+
+
 fclose($fn);
 
